5c9a0b3ca356b8dcfdea132ccc3cd8cb9cd4896e
diegooli <dev.diego.oliveira@gmail.com>
2022-12-13 19:39:08 -0300

    Added added modifications

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
-const form = document.getElementById("form");
+const form = document.getElementById('contact-form');
 
-function handleSumit(event){
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    
+
     const name = form.elements['name'].value;
     const email = form.elements['email'].value;
     const message = form.elements['message'].value;
-
-    const result = name + " "+email + " " + message;
-    const result2 = `Nome: ${name}\nE-mail: ${email}\nMensagem: ${message}\n`;
-
-
-    alert(result2)
-}
-
-form.addEventListener("submit", handleSumit)
+    
+    const result = `Nome: ${name}\nE-mail: ${email}\nMessage: ${message}`;
+    alert(result);
+});
